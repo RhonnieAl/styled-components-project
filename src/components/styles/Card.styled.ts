b8bcb8a8e2f13efe69fca2ea66b1efe35fc73543
Rhonnie Al <rhonnieallan@protonmail.com>
2023-11-19 22:08:25 +0200

    Add Card styling: Centre Cards on Desktop

--- src/components/styles/Card.styled.ts
+++ src/components/styles/Card.styled.ts
@@ -10,14 +10,15 @@ export const StyledCard = styled.div<StyledCardProps>`
   background-color: #fff;
   border-radius: 15px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  margin: 40px 0;
+  margin: 40px auto; // Changed to auto to center the cards & maintain the vertical spacing
   padding: 60px;
+  max-width: 70%; // Added this to limit the width of the cards. They were too wide on Desktop
   flex-direction: ${(props) =>
     props.layout ||
     "row"}; // This reverses the direction, so that every other image card is on the right side of the text or the left
 
   img {
-    width: 50%;
+    width: 80%;
   }
 
   > div {
@@ -27,5 +28,6 @@ export const StyledCard = styled.div<StyledCardProps>`
   @media (max-width: ${({ theme }) => theme.mobile}) {
     flex-direction: column;
     text-align: center;
+    max-width: 100%; // Added this b/c I prefer the cards to be 100% width on mobile
   }
 `;
